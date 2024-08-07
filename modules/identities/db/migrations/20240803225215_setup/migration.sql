rivet-gg/modules

feat: Create the `auth_provider` module

modules/auth_providers/db/migrations/20240629231731_init/migration.sql
@@ -1,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "ProviderEntries" (
+    "userId" UUID NOT NULL,
+    "providerType" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "providerData" JSONB NOT NULL,
+
+    CONSTRAINT "ProviderEntries_pkey" PRIMARY KEY ("userId","providerType","providerId")
+);
+
+-- CreateIndex
+CREATE INDEX "ProviderEntries_userId_idx" ON "ProviderEntries"("userId");
+
+-- CreateIndex
+CREATE INDEX "ProviderEntries_providerType_providerId_idx" ON "ProviderEntries"("providerType", "providerId");

rivet-gg/opengb-modules

modules/identities/db/migrations/20240803225215_setup/migration.sql
@@ -1,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "UserIdentities" (
+    "userId" UUID NOT NULL,
+    "identityType" TEXT NOT NULL,
+    "identityId" TEXT NOT NULL,
+    "uniqueData" JSONB NOT NULL,
+    "additionalData" JSONB NOT NULL,
+
+    CONSTRAINT "UserIdentities_pkey" PRIMARY KEY ("userId","identityType","identityId")
+);
+
+-- CreateIndex
+CREATE INDEX "UserIdentities_userId_idx" ON "UserIdentities"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserIdentities_identityType_identityId_uniqueData_key" ON "UserIdentities"("identityType", "identityId", "uniqueData");